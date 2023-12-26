f342a79a86e06e81a3aff890d6703d03c9487b0d
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-12-26 14:26:18 +0530

    Port changed to 3001

--- algohype_backend/index.js
+++ algohype_backend/index.js
@@ -64,7 +64,7 @@ app.post('/process', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
